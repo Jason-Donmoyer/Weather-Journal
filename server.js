ac253bcb77938c6bdd6ebcac59806abd253f302c
Jason-Donmoyer <j.donmoyer523@gmail.com>
2020-09-19 04:41:56 -0400

    set up server

--- server.js
+++ server.js
@@ -2,18 +2,27 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+app.listen('3000', () => {
+  console.log('Serving up good some good shit!');
+});
--- website/app.js
+++ website/app.js
@@ -1,5 +1,8 @@
 /* Global Variables */
 
+const url;
+const APIKey = '13113e1d1128465c8ea19117b412794e';
+
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
